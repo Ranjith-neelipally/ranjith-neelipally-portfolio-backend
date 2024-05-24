e66284d17f46aa36925cb9595784c1c58760013d
ranjithsam21 <ranjithsam21@gmail.com>
2024-05-24 19:34:25 +0000

    added db

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("./database");
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8083;
 app.get('/', (_req, res) => {
     return res.json({ message: 'Hello World!' });
+});
+app.get('/check', (req, res) => {
+    if (mongoose_1.default.connection.readyState === 1) {
+        res.json({ message: "connected" });
+    }
+    else {
+        res.json({ message: "not connected" });
+    }
 });
 app.listen(port, () => {
     return console.log(`Server is listening on http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express, { Request, Response } from 'express'
 import "./database"
+import mongoose from "mongoose";
+
 
 const app = express()
 const port = process.env.PORT || 8083
 
 app.get('/', (_req: Request, res: Response) => {
 	return res.json({ message: 'Hello World!' })
+})
+
+app.get('/check', (req:Request, res:Response )=>{
+	if(mongoose.connection.readyState===1){
+		res.json({message:"connected"})
+	}else{
+		res.json({message:"not connected"})
+	}
 })
 
 app.listen(port, () => {
