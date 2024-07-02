0649312180d58c8fcfb5bdb9791f4a46a23664fb
Ranjith <ranjith.neelipally@ibigroup.com>
2024-07-02 15:45:49 +0530

    login flow

---
+++ dist/utils/Schema/LoginValidation/index.js
@@ -0,0 +1,41 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoginValidationSchema = void 0;
+const yup = __importStar(require("yup"));
+const PasswordvalidationExpression = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]{8,}$/;
+exports.LoginValidationSchema = yup.object().shape({
+    email: yup
+        .string()
+        .trim()
+        .required("email is required")
+        .email("invalid email id!"),
+    password: yup
+        .string()
+        .trim()
+        .required("Password is missing")
+        .min(8, "Password is too short!"),
+});
+//# sourceMappingURL=index.js.map
