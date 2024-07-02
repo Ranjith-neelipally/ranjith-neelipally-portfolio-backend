43195a60ac9f104830fea207b57514fb79ab19c9
Ranjith <ranjith.neelipally@ibigroup.com>
2024-07-02 15:46:42 +0530

    sample

--- dist/utils/variables.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOKEN_KEY = exports.MONGO_URI = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const { env } = process;
-exports.MONGO_URI = env.MONGO_URI, exports.TOKEN_KEY = env.TOKEN_KEY;
-//# sourceMappingURL=variables.js.map
