b8ec2330f8c4abdcb7b46ec5fe2ec354950fa1e7
Ranjith <ranjithsam21@gmail.com>
2026-06-15 12:24:25 +0530

    removed build

--- dist/utils/variables.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BLOB_READ_WRITE_TOKEN = exports.ADMIN_MAIL = exports.MAILTRAP_PASSWORD = exports.MAILTRAP_USER = exports.MAIL_TRAP_TOKEN = exports.TOKEN_KEY = exports.MONGO_URI = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const { env } = process;
-exports.MONGO_URI = env.MONGO_URI, exports.TOKEN_KEY = env.TOKEN_KEY, exports.MAIL_TRAP_TOKEN = env.MAIL_TRAP_TOKEN, exports.MAILTRAP_USER = env.MAILTRAP_USER, exports.MAILTRAP_PASSWORD = env.MAILTRAP_PASSWORD, exports.ADMIN_MAIL = env.ADMIN_MAIL, exports.BLOB_READ_WRITE_TOKEN = env.BLOB_READ_WRITE_TOKEN;
