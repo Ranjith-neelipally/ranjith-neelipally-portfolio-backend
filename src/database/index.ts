5a3966449628788b1f2adeb437feff3de2d23ee8
Ranjith <ranjithsam21@gmail.com>
2026-06-15 12:47:44 +0530

    updated batabase connection file

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,8 +3,15 @@ import { MONGO_URI } from "../utils/variables";
 
 const URI = MONGO_URI as string;
 
+if (!URI) {
+  console.error("MONGO_URI environment variable is not defined");
+}
+
+// Connect with serverless optimizations (disable buffering)
 mongoose
-  .connect(URI)
+  .connect(URI, {
+    bufferCommands: false,
+  })
   .then(() => {
     console.log("Connected to MongoDB");
   })
